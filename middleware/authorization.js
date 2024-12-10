ac091ebcbe8352e0f69586030a383b7c28dc9e0e
18319027 <18319027@std.stei.itb.ac.id>
2024-12-10 14:54:14 +0700

    Add console log

--- middleware/authorization.js
+++ middleware/authorization.js
@@ -2,11 +2,13 @@ const jwt = require('jsonwebtoken');
 
 const authorization = (req, res, next) => {
     const token = req.cookies.jwtToken;
+    console.log("token di cookies: ", token);
     if (!token) {
-      return res.status(401).send("AUTHORIZATION ERROR!!!");
+      return res.status(401).send("Please provide your authentication token!");
     }
     try {
       let payload = jwt.verify(token, process.env.JWT_SECRET);
+      console.log("payload : ", payload);
       return next();
     } 
     catch {
