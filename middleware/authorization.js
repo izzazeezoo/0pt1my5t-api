6f8f7a4d318c0a73668c9039591ea154588c4633
18319027 <18319027@std.stei.itb.ac.id>
2024-12-12 16:56:53 +0700

    add 'TokenExpiredError' case

--- middleware/authorization.js
+++ middleware/authorization.js
@@ -1,22 +1,22 @@
 const jwt = require("jsonwebtoken");
 
 const authorization = (req, res, next) => {
-	const token =
-		req.cookies.jwtToken || req.headers.authorization?.split(" ")[1];
-	console.log("current token : ", token);
-	if (!token) {
-		return res.status(401).send("Unauthorized: No token provided!");
-	}
-	try {
-		let payload = jwt.verify(token, process.env.JWT_SECRET);
-		console.log("payload : ", payload);
-		return next();
-	} catch (err) {
+  const token =
+    req.cookies.jwtToken || req.headers.authorization?.split(" ")[1];
+  console.log("current token : ", token);
+  if (!token) {
+    return res.status(401).send("Unauthorized: No token provided!");
+  }
+  try {
+    let payload = jwt.verify(token, process.env.JWT_SECRET);
+    console.log("payload : ", payload);
+    return next();
+  } catch (err) {
     if (err.name === 'TokenExpiredError') {
-        return res.status(401).send("Session expired. Please log in again.");
+      return res.status(401).send("Session expired. Please log in again.");
     }
     return res.status(401).send("Invalid token");
-}
+  }
 };
 
 module.exports = authorization;
